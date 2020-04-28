a25b7768b49fc5a2e9d6962e4a851c79225f5463
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-04-28 14:07:49 +0200

    Create data source for tasks list

--- QuickTodo/Controllers/TasksViewController.swift
+++ QuickTodo/Controllers/TasksViewController.swift
@@ -39,15 +39,34 @@ class TasksViewController: UIViewController, BindableType {
   @IBOutlet var newTaskButton: UIBarButtonItem!
   
   var viewModel: TasksViewModel!
+  var dataSource: RxTableViewSectionedAnimatedDataSource<TaskSection>!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     tableView.rowHeight = UITableView.automaticDimension
     tableView.estimatedRowHeight = 60
+    
+    configureDataSource()
+  }
+  
+  private func configureDataSource() {
+    dataSource = RxTableViewSectionedAnimatedDataSource<TaskSection>(configureCell: {
+      [weak self] dataSource, tableView, indexPath, item in
+      let cell = tableView.dequeueReusableCell(withIdentifier: "TaskItemCell", for: indexPath) as! TaskItemTableViewCell
+      if let self = self {
+        cell.configure(with: item, action: self.viewModel.onToggle(task: item))
+      }
+      return cell
+      },
+      titleForHeaderInSection: { dataSource, index in
+        dataSource.sectionModels[index].model
+      })
   }
   
   func bindViewModel() {
-
+    viewModel.sectionedItems
+      .bind(to: tableView.rx.items(dataSource: dataSource))
+      .disposed(by: self.rx.disposeBag)
   }
 }

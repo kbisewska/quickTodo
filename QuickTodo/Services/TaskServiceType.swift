5604fe801018bbc44ce7c379cdbf316942409c73
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-04-28 12:47:41 +0200

    Create TaskServiceType protocol

--- QuickTodo/Services/TaskServiceType.swift
+++ QuickTodo/Services/TaskServiceType.swift
@@ -31,6 +31,7 @@ import RxSwift
 import RealmSwift
 
 enum TaskServiceError: Error {
+  
   case creationFailed
   case updateFailed(TaskItem)
   case deletionFailed(TaskItem)
@@ -38,5 +39,18 @@ enum TaskServiceError: Error {
 }
 
 protocol TaskServiceType {
-	
+  
+  @discardableResult
+  func createTask(title: String) -> Observable<TaskItem>
+  
+  @discardableResult
+  func delete(task: TaskItem) -> Observable<Void>
+  
+  @discardableResult
+  func update(task: TaskItem, title: String) -> Observable<TaskItem>
+  
+  @discardableResult
+  func toggle(task: TaskItem) -> Observable<TaskItem>
+  
+  func tasks() -> Observable<Results<TaskItem>>
 }

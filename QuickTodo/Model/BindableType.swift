742426335adb974047f9c123ffc4dbc6b5234e2e
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-04-28 12:39:45 +0200

    Create BindableType protocol

--- QuickTodo/Model/BindableType.swift
+++ QuickTodo/Model/BindableType.swift
@@ -29,3 +29,20 @@
 import UIKit
 import RxSwift
 
+protocol BindableType {
+  
+  associatedtype ViewModelType
+  
+  var viewModel: ViewModelType! { get set }
+  
+  func bindViewModel()
+}
+
+extension BindableType where Self: UIViewController {
+  
+  mutating func bindViewModel(to model: Self.ViewModelType) {
+    viewModel = model
+    loadViewIfNeeded()
+    bindViewModel()
+  }
+}
